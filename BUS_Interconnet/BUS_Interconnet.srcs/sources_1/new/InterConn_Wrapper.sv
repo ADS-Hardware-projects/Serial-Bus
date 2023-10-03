9dfd355552fa6cb2a8db3c5089054434c94768a6
0301yasiru <0301yasiru@gmail.com>
2023-10-03 23:31:13 +0530

    Wrapper Module Created and started working on that

---
+++ BUS_Interconnet/BUS_Interconnet.srcs/sources_1/new/InterConn_Wrapper.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/03/2023 11:30:20 PM
+// Design Name: 
+// Module Name: InterConn_Wrapper
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module InterConn_Wrapper(
+
+    );
+endmodule
